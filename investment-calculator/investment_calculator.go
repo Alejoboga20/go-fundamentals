11cdafeabc97479530736c494e32496cf3d29514
Alejoboga20 <alejoboga19@gmail.com>
2024-08-15 15:31:24 -0300

    adding comments

--- investment-calculator/investment_calculator.go
+++ investment-calculator/investment_calculator.go
@@ -9,7 +9,9 @@ func main() {
 	// We can use `const` to define constants. Constants are immutable and their value cannot be changed
 	const inflationRate = 2.5
 	// We can define more than one variable in the same line. We can use the same type of inference for multiple variables
-	var investmentAmount, years, expectedReturnRate float64
+	var investmentAmount, years float64
+	// we can init a variable with a value using `:=` for inference
+	expectedReturnRate := 5.5
 
 	fmt.Print("Enter the investment amount: ")
 	// We need to use `&` to get the memory address of the variable. Scan will read the input from the user and store it in the memory address of the variable
@@ -18,6 +20,7 @@ func main() {
 	fmt.Print("Enter the number of years you'd like to invest: ")
 	fmt.Scan((&years))
 
+	// Note: `fmt.Scan` only allows us to read one input at a time or simple words
 	fmt.Print("Enter the expected return rate: ")
 	fmt.Scan((&expectedReturnRate))
 
