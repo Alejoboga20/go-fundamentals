aca5bbf472eae48efba90027b48e6ffba3a3ef13
Alejoboga20 <alejoboga19@gmail.com>
2024-08-19 12:02:29 -0300

    invetment calculator

---
+++ profit-calculator/profit_calculator.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var revenue, expenses, taxRate float64
+
+	fmt.Print("Enter the revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("Enter the expenses: ")
+	fmt.Scan(&expenses)
+
+	fmt.Print("Enter the tax rate: ")
+	fmt.Scan(&taxRate)
+
+	earnings__before_tax := revenue - expenses
+	profit := earnings__before_tax * (1 - taxRate/100)
+	ratio := earnings__before_tax / profit
+
+	fmt.Println("Earnings before tax: ", earnings__before_tax)
+	fmt.Println("Profit: ", profit)
+	fmt.Println("Ratio: ", ratio)
+}
