686448213da6bd1bce70f66d17008c805ec5d5f4
Alejoboga20 <alejoboga19@gmail.com>
2024-09-03 14:09:10 -0300

    multiple ifs

--- control-structures/bank.go
+++ control-structures/bank.go
@@ -24,4 +24,23 @@ func main() {
 	if choice == 1 {
 		fmt.Println("Your balance is $", accountBalance)
 	}
+
+	if choice == 2 {
+		var depositAmount float64
+		fmt.Print("Enter the amount you want to deposit: ")
+
+		fmt.Scan(&depositAmount)
+		accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
+
+		fmt.Println("Your new balance is $", accountBalance)
+	}
+
+	if choice == 3 {
+		var withdrawAmount float64
+		fmt.Println("Enter the amount you want to withdraw: ")
+
+		fmt.Scan(&withdrawAmount)
+		accountBalance -= withdrawAmount // accountBalance = accountBalance - withdrawAmount
+		fmt.Println("Your new balance is $", accountBalance)
+	}
 }
