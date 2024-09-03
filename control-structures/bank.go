e21ab73ff3a1b37aa554447be96fa58d546d0f39
Alejoboga20 <alejoboga19@gmail.com>
2024-09-03 14:29:20 -0300

    for loop

--- control-structures/bank.go
+++ control-structures/bank.go
@@ -9,51 +9,55 @@ func main() {
 	var accountBalance = 1000.0
 
 	fmt.Println("Welcome to Go Bank!")
-	fmt.Println("What woud you like to do?")
 
-	fmt.Println("1. Check Balance")
-	fmt.Println("2. Deposit Money")
-	fmt.Println("3. Withdraw Money")
-	fmt.Println("4. Exit")
+	for i := 0; i != 4; i = choice {
+		fmt.Println("What woud you like to do?")
 
-	fmt.Print("Your choice: ")
-	fmt.Scan(&choice)
+		fmt.Println("1. Check Balance")
+		fmt.Println("2. Deposit Money")
+		fmt.Println("3. Withdraw Money")
+		fmt.Println("4. Exit")
 
-	// wantsToCheckBalance := choice == 1
+		fmt.Print("Your choice: ")
+		fmt.Scan(&choice)
 
-	if choice == 1 {
-		fmt.Println("Your balance is $", accountBalance)
-	} else if choice == 2 {
-		var depositAmount float64
+		// wantsToCheckBalance := choice == 1
 
-		fmt.Print("Enter the amount you want to deposit: ")
-		fmt.Scan(&depositAmount)
+		if choice == 1 {
+			fmt.Println("Your balance is $", accountBalance)
+		} else if choice == 2 {
+			var depositAmount float64
 
-		if depositAmount <= 0 {
-			fmt.Println("You cannot deposit a negative amount")
-			return
+			fmt.Print("Enter the amount you want to deposit: ")
+			fmt.Scan(&depositAmount)
+
+			if depositAmount <= 0 {
+				fmt.Println("You cannot deposit a negative amount")
+				return
+			}
+
+			accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
+			fmt.Println("Your new balance is $", accountBalance)
+		} else if choice == 3 {
+			var withdrawAmount float64
+			fmt.Println("Enter the amount you want to withdraw: ")
+
+			fmt.Scan(&withdrawAmount)
+
+			if withdrawAmount <= 0 {
+				fmt.Println("You cannot withdraw a negative amount")
+				return
+			} else if withdrawAmount > accountBalance {
+				fmt.Println("You do not have enough balance to withdraw that amount")
+				return
+			}
+
+			accountBalance -= withdrawAmount // accountBalance = accountBalance - withdrawAmount
+			fmt.Println("Your new balance is $", accountBalance)
+		} else if choice == 4 {
+			fmt.Println("Goodbye!")
+		} else {
+			fmt.Println("Invalid choice. Please try again.")
 		}
-
-		accountBalance += depositAmount // accountBalance = accountBalance + depositAmount
-		fmt.Println("Your new balance is $", accountBalance)
-	} else if choice == 3 {
-		var withdrawAmount float64
-		fmt.Println("Enter the amount you want to withdraw: ")
-
-		fmt.Scan(&withdrawAmount)
-
-		if withdrawAmount <= 0 {
-			fmt.Println("You cannot deposit a negative amount")
-			return
-		} else if withdrawAmount > accountBalance {
-			fmt.Println("You do not have enough balance to withdraw that amount")
-			return
-		}
-
-		accountBalance -= withdrawAmount // accountBalance = accountBalance - withdrawAmount
-		fmt.Println("Your new balance is $", accountBalance)
-	} else {
-		fmt.Println("Goodbye!")
 	}
-
 }
