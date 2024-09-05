a7eb8ad559a73010db3cf981ca97dd20adc10793
Alejoboga20 <alejoboga19@gmail.com>
2024-09-05 12:14:52 -0300

    pointers with structs

--- 04-structs/structs.go
+++ 04-structs/structs.go
@@ -32,6 +32,7 @@ func main() {
 	}
 
 	outputUserDetails(appUser)
+	outputUserWithPointer(&appUser)
 }
 
 func outputUserDetails(appUser User) {
@@ -39,6 +40,14 @@ func outputUserDetails(appUser User) {
 	fmt.Println("Last Name:", appUser.lastName)
 	fmt.Println("Birthdate:", appUser.birthdate)
 	fmt.Println("Created At:", appUser.createdAt)
+}
+
+func outputUserWithPointer(appUser *User) {
+	// when using pointer with structs there is no need to use (*appUser).fieldName
+	fmt.Println("First Name With Pointer:", appUser.firstName)
+	fmt.Println("Last Name With Pointer:", appUser.lastName)
+	fmt.Println("Birthdate With Pointer:", appUser.birthdate)
+	fmt.Println("Created At With Pointer:", appUser.createdAt)
 }
 
 func getUserData(promptText string) string {
