18f8d036d8ad7348c3cde63ada51d1d117561277
alejoboga-io <alejo.boga@pendo.io>
2024-09-17 11:58:22 -0300

    create arrays

--- 05-collections/lists.go
+++ 05-collections/lists.go
@@ -3,7 +3,12 @@ package main
 import "fmt"
 
 func main() {
+	productNames := [4]string{"laptop", "mouse", "keyboard"}
 	prices := []float64{1.50, 2.35, 3.123, 4, 5}
+
+	fmt.Println(productNames)
+	productNames[0] = "monitor"
+	fmt.Println(productNames)
 
 	for i := range prices {
 		fmt.Println(prices[i])
