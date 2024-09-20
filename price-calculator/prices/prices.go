6872c299c49ba986a85a6d4ed3da8c9116e0cf76
alejoboga-io <alejo.boga@pendo.io>
2024-09-20 11:54:53 -0300

    reading file

--- price-calculator/prices/prices.go
+++ price-calculator/prices/prices.go
@@ -1,6 +1,10 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
@@ -8,7 +12,34 @@ type TaxIncludedPriceJob struct {
 	TaxIncludedPrices map[string][]float64
 }
 
-func (job TaxIncludedPriceJob) Process() {
+func (job *TaxIncludedPriceJob) LoadData() {
+	var lines []string
+	file, err := os.Open("prices.txt")
+
+	if err != nil {
+		fmt.Println("An error occurred while opening the file")
+		fmt.Println(err)
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	err = scanner.Err()
+
+	if err != nil {
+		fmt.Println("An error occurred while reading the file")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(lines)
+}
+
+func (job *TaxIncludedPriceJob) Process() {
 	result := make(map[string]float64)
 
 	for _, price := range job.InputPrices {
