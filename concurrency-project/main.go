89c78573ba2c16d00805c54cd5d5f04228493127
alejoboga-io <alejo.boga@pendo.io>
2024-09-24 16:23:50 -0300

    create error channel and select

--- concurrency-project/main.go
+++ concurrency-project/main.go
@@ -10,16 +10,26 @@ import (
 func main() {
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 	doneChannels := make([]chan bool, len(taxRates))
+	errorChannels := make([]chan error, len(taxRates))
 
 	for index, taxRate := range taxRates {
 		doneChannels[index] = make(chan bool)
+		errorChannels[index] = make(chan error)
+
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 
 		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
-		go priceJob.Process(doneChannels[index])
+		go priceJob.Process(doneChannels[index], errorChannels[index])
 	}
 
-	for _, doneChan := range doneChannels {
-		<-doneChan
+	for index, _ := range taxRates {
+		select {
+		case err := <-errorChannels[index]:
+			if err != nil {
+				fmt.Println(err)
+			}
+		case <-doneChannels[index]:
+			fmt.Println("Done")
+		}
 	}
 }
